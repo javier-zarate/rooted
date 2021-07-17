d428b5289ff65416f8cb6677664de3f360477479
Javier Zarate <javalzar17@gmail.com>
2021-07-16 21:16:27 -0700

    testing graphQL query to shopify data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,49 @@
 const path = require('path');
 
 // Where static files will be generated and creating unique products stored within shopify
+// allows to add additional config to webpack to allow to
+// import modules absolutely instead of relatively
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     },
+  });
+};
+
+// Gatsby expects create pages
+// Gatsby knows to inject various helpers to generate pages
+// helpers:
+// actions: query data via graphql
+// actions: refers to redux actions
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  // this graphql function is a promise
+  const { data } = await graphql(`
+    {
+      allShopifyProduct {
+        edges {
+          node {
+            shopifyId
+            handle
+          }
+        }
+      }
+    }
+  `);
+  // Summary: query all shopify products
+
+  // for Each of the shopify products a new page will be created
+  // accessible at /products/${ product handle}
+  // a component will render for each of the products
+  data.allShopifyProduct.edges.forEach(({ node }) => {
+    createPage({
+      path: `products/${node.handle}`,
+      context: {
+        shopifyId: node.shopifyId,
+      },
+      component: path.resolve('./src/templates/ProductTemplate/index.js'),
+    });
   });
 };
---
+++ src/templates/ProductTemplate/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Layout } from 'components';
+import { Grid } from './styles';
+
+// this is a tagged template literal
+// exporting a query in gatsby like this is called a "page query"
+// anytime context is passed to component to render for particular page
+// you gain access to that variable or that context within the graphql query itself
+export const query = graphql`
+  query ProductQuery($shopifyId: String) {
+    shopifyProduct(shopifyId: { eq: $shopifyId }) {
+      title
+      description
+      images {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+// When you export a page query like above
+// Gatsby will inject the result into props under data
+export default function ProductTemplate({ data }) {
+  console.log(data);
+  return (
+    <Layout>
+      <Grid>
+        <div>
+          <h1>{data.shopifyProduct.title}</h1>
+          <p>{data.shopifyProduct.description}</p>
+        </div>
+        <div>IMAGES</div>
+      </Grid>
+    </Layout>
+  );
+}
