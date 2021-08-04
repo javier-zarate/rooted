a04dd71b0e951fc16f2740d6b40aa44a91e97a4e
Javier Zarate <javalzar17@gmail.com>
2021-08-03 21:57:34 -0700

    products page renders product info (title, description etc) and product images. Image gallery has been implemented and current active product will render as main image and is highlighted in gallery.

--- src/components/ImageGallery/ImageThumbnail/index.js
+++ src/components/ImageGallery/ImageThumbnail/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import Image from 'gatsby-image';
 import { ImageThumbnailWrapper } from './styles';
 
-
 // active prop -> When a image thumbnail is active it will be highlighed
 // image prop -> to render prop
 // onclick -> to highlight current image
-export default function ImageThumbnail({ isactive, onClick, image }) {
+export default function ImageThumbnail({ isActive, onClick, image }) {
+  const handleClick = () => {
+    onClick(image);
+  };
+
   return (
-    <ImageThumbnailWrapper
-      onClick={() => {
-        console.log('THOU IMAGE HAS BEEN CLICKETH');
-      }}
-    >
-      <Image fluid={image} />
+    <ImageThumbnailWrapper onClick={handleClick} isActive={isActive}>
+      <Image fluid={image.localFile.childImageSharp.fluid} />
     </ImageThumbnailWrapper>
   );
 }
--- src/components/ImageGallery/index.js
+++ src/components/ImageGallery/index.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'gatsby-image';
 import { ImageGalleryWrapper } from './styles';
 import ImageThumbnail from './ImageThumbnail';
 
 export function ImageGallery({ images }) {
+  const [activeImageThumbnail, setActiveImageThumbnail] = useState(images[0]);
+  const handleClick = image => {
+    setActiveImageThumbnail(image);
+  };
+
   return (
     <ImageGalleryWrapper>
       <div>
-        <Image fluid={images[0].localFile.childImageSharp.fluid} />
+        <Image fluid={activeImageThumbnail.localFile.childImageSharp.fluid} />
       </div>
       <div>
         {images.map(image => {
           return (
             <ImageThumbnail
               key={image.id}
-              image={image.localFile.childImageSharp.fluid}
+              isActive={activeImageThumbnail.id === image.id}
+              onClick={handleClick}
+              image={image}
             />
           );
         })}
