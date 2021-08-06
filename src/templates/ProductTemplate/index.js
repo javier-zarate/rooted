2be073fd8bc7427e74a7cbc2cc527c749120ca02
Javier Zarate <javalzar17@gmail.com>
2021-08-06 14:12:39 -0700

    variantId is now added as a query string to URL creating a URI where the user can share the link and it will load on that particular product and variant.

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/no-onchange */
 import React, { useContext, useEffect, useState } from 'react';
 import { graphql } from 'gatsby';
 import { Layout, ImageGallery } from 'components';
 import { Grid, SelectWrapper, Price } from './styles';
 import CartContext from 'context/CartContext';
 import { navigate, useLocation } from '@reach/router';
+import queryString from 'query-string';
 
 // this is a tagged template literal
 // exporting a query in gatsby like this is called a "page query"
@@ -36,37 +38,42 @@ export default function ProductTemplate({ data }) {
   const [product, setProduct] = useState(null);
   const [selectedVariant, setSelectedVariant] = useState(null);
   const { search, origin, pathname } = useLocation();
-  console.log(search, origin, pathname);
+  const variantId = queryString.parse(search).variant;
+
+  // console.log(search, origin, pathname);
 
   useEffect(() => {
     getProductById(data.shopifyProduct.shopifyId).then(result => {
       setProduct(result);
-      setSelectedVariant(result.variants[0]);
+      setSelectedVariant(
+        result.variants.find(({ id }) => id === variantId) || result.variants[0]);
     });
-  }, [getProductById, setProduct, data.shopifyProduct.shopifyId]);
+  }, [getProductById, setProduct, data.shopifyProduct.shopifyId, variantId]);
 
   const handleVariantChange = e => {
     const newVariant = product?.variants.find(
       item => item.id === e.target.value
     );
     setSelectedVariant(newVariant);
-    navigate(`${origin}${pathname}?variant=${encodeURIComponent(newVariant)}`, {
+    navigate(`${origin}${pathname}?variant=${encodeURIComponent(newVariant.id)}`, {
       replace: true,
     });
   };
+
   return (
     <Layout>
       <Grid>
         <div>
           <h1>{data.shopifyProduct.title}</h1>
           <p>{data.shopifyProduct.description}</p>
-          {product?.availableForSale && (
+          {product?.availableForSale && !!selectedVariant && (
             <>
               <SelectWrapper>
                 <strong>Variant</strong>
                 <select
                   onChange={handleVariantChange}
-                  onBlur={handleVariantChange}
+                  // onBlur={handleVariantChange}
+                  value={selectedVariant.id}
                 >
                   {product?.variants.map(item => (
                     <option key={item.id} value={item.id}>
