98fecdd05d6eb45a60fe239044a39eb278db3752
Javier Zarate <javalzar17@gmail.com>
2021-08-13 14:56:49 -0700

    added logic to add to cart form and button. Working in implementing header to update as cart quantity changes.

--- src/components/ProductQuantityAdder/index.js
+++ src/components/ProductQuantityAdder/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProductQuantityAdderWrapper } from './styles';
 import { Button } from '../Button';
 import { Input } from '../Input';
 
 export function ProductQuantityAdder({ available, variantId }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = e => {
+    setQuantity(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <ProductQuantityAdderWrapper>
       <strong>Quantity</strong>
-      <form>
-        <Input />
-        <Button fullWidth>Add to cart</Button>
+      <form onSubmit={handleSubmit}>
+        <Input
+          disabled={!available}
+          type="number"
+          min="1"
+          step="1"
+          value={quantity}
+          onChange={handleQuantityChange}
+        />
+        <Button type="submit" disabled={!available} fullWidth>
+          Add to cart
+        </Button>
       </form>
     </ProductQuantityAdderWrapper>
   );
