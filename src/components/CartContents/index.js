88f19d420ec197afd2dc16ae4378d73057adab04
Javier Zarate <javalzar17@gmail.com>
2021-09-07 22:37:23 -0700

    added checkout utilizing shopifyAPI.

--- src/components/CartContents/index.js
+++ src/components/CartContents/index.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import CartContext from 'context/CartContext';
-import { CartItem, CartHeader, CartFooter } from './styles';
+import { CartItem, CartHeader, CartFooter, Footer } from './styles';
 import { QuantityAdjuster } from '../QuantityAdjuster';
 import { RemoveLineItem } from '../RemoveLineItem';
+import { Button } from '../Button';
+import { navigate } from '@reach/router';
 
 export function CartContents() {
   const { checkout, updateLineItem } = useContext(CartContext);
@@ -14,12 +16,14 @@ export function CartContents() {
   return (
     <section>
       <h1>Your Cart</h1>
-      <CartHeader>
-        <div>Product</div>
-        <div>Unit Price</div>
-        <div>Quantity</div>
-        <div>Amount</div>
-      </CartHeader>
+      {!!checkout?.lineItems && (
+        <CartHeader>
+          <div>Product</div>
+          <div>Unit Price</div>
+          <div>Quantity</div>
+          <div>Amount</div>
+        </CartHeader>
+      )}
       {checkout?.lineItems?.map(item => (
         <CartItem key={item.variant.id}>
           <div>
@@ -38,12 +42,33 @@ export function CartContents() {
           </div>
         </CartItem>
       ))}
+      {!!checkout?.lineItems && (
       <CartFooter>
         <div>
           <strong>Total:</strong>
         </div>
         <div>${checkout?.totalPrice}</div>
       </CartFooter>
+      )}
+      {!checkout?.lineItems && (
+        <h4>Your cart is empty. <br />Treat yourself and add something.</h4>
+      )}
+      <Footer>
+          <div>
+            <Button onClick={() => navigate(-1)}>Continue shopping</Button>
+          </div>
+          <div>
+            {!!checkout?.webUrl && (
+              <Button
+                onClick={() => {
+                  window.location.href = checkout.webUrl;
+                }}
+              >
+                Checkout
+              </Button>
+            )}
+          </div>
+        </Footer>
     </section>
   );
 }
